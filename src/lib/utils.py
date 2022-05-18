764d0c76be1c8edca63d338f75a8a2bdcb56c477
Sanjay Kumar <sanjay.kumar@hometrack.com.au>
2022-05-18 21:44:08 +1000

    read from files

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -2,6 +2,10 @@ from ast import parse
 from typing import Dict, List
 from datetime import datetime
 from models.input_row import InputRow
+import logging
+from pathlib import Path
+
+logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
 
 
 def list_parse_map(input: List) -> Dict:
@@ -17,3 +21,15 @@ def list_parse_map(input: List) -> Dict:
   res_dct = dict(zip(keywords_iter, input_iter))
   
   return res_dct
+
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+
+def read_files(dir: str) -> List[str]:
+  output = []
+  for p in Path(dir).glob('**/*.txt'):
+    logging.info(f"Processing {p.name}...")
+    output.append(p.read_text().rstrip().splitlines())
+  return flatten(output)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import logging
-from lib.utils import list_parse_map
+from lib.utils import list_parse_map, read_files
 from models.input import Input
 from models.input_row import InputRow
+
+DATA_DIR = "./data/"
 
 logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
 logging.info("Starting Traffic Counter...")
 
-sample_data = "2021-12-01T05:00:00 5".split(" ")
-sample_map = list_parse_map(sample_data)
+line_rows = []
+data_lines = read_files(dir=DATA_DIR)
+for line in data_lines:
+  line_data = line.split(" ")
+  line_map = list_parse_map(line_data)
+  line_row = InputRow(**line_map)
+  line_rows.append(line_row)
 
-sample_row = InputRow(**sample_map)
-sample_output = Input(**{"windows": [sample_row]})
-logging.debug(sample_output)
+line_output = Input(**{"windows": line_rows})
+logging.debug(line_output)
 
 logging.info("Stopping Traffic Counter...")
