afc2945aa76aede440b7a3c27a0b17280849c2fd
Sanjay Kumar <sanjay.kumar@hometrack.com.au>
2022-05-18 22:35:46 +1000

    answer questions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import logging
 from lib.utils import read_files
-from models.input import Input
 from transformers.input_row_transformer import row_to_input_row
 from transformers.input_transformer import input_rows_to_input
 
@@ -9,13 +8,33 @@ DATA_DIR = "./data/"
 logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
 logging.info("Starting Traffic Counter...")
 
-line_rows = []
-data_lines = read_files(dir=DATA_DIR)
-for line in data_lines:
-  line_row = row_to_input_row(line)
-  line_rows.append(line_row)
+# Read all files in the DATA_DIR. 
+data_rows = read_files(dir=DATA_DIR)
 
-line_output = input_rows_to_input(line_rows)
-logging.debug(line_output)
+# Transform all text into input model. 
+input_rows = []
+for row in data_rows:
+  input_row = row_to_input_row(row)
+  input_rows.append(input_row)
+input = input_rows_to_input(input_rows)
+
+# Calculating necessary statistics:
+
+# 1. The number of cars seen in total
+sum = 0
+for input_row in input.windows:
+  sum += input_row.window_count
+logging.debug(f"The number of cars seen in total: {sum}")
+
+# 2. A sequence of lines where each line contains a date (in yyyy-mm-dd format) and the
+# number of cars seen on that day (eg. 2016-11-23 289) for all days listed in the input file.
+res = {}
+for input_row in input.windows:
+  input_date = str(input_row.window_datetime.date())
+  if input_date in res:
+    res[input_date] += input_row.window_count
+  else:
+    res[input_date] = input_row.window_count
+logging.debug(f"A sequence of lines where each line contains a date (in yyyy-mm-dd format) and the number of cars seen on that day: \n {res}")
 
 logging.info("Stopping Traffic Counter...")
