85dc9b7b28ddb14592fa79bc054d578a1528298c
Sanjay Kumar <sanjay.kumar@hometrack.com.au>
2022-05-21 09:36:49 +1000

    add test for read files

--- test/lib/test_utils.py
+++ test/lib/test_utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from unittest.mock import patch
+import src
 from src.lib.utils import list_parse_map, flatten
-
 
 def test_list_parse_map():
   test_input = ["2021-12-01T05:00:00", "5"]
@@ -14,3 +15,11 @@ def test_flatten():
   test_input = ["1", ["2", "3"]]
   expected_output = ["1", "2", "3"]
   assert flatten(test_input) == expected_output
+
+@patch('src.lib.utils.read_files')
+def test_read_files(mocked_read_files):
+  test_input = "foo_dir"
+  mocked_read_files(test_input)
+  assert mocked_read_files.called
+  assert mocked_read_files.called_with(test_input)
+  assert mocked_read_files is src.lib.utils.read_files
